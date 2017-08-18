51531e36d8f8e79d76e219b1330b959b0a21f8af
santosg57 <lgs@unam.mx>
2017-08-18 11:04:02 -0500

    Update Load_fMRI_data.py

--- Load_fMRI_data.py
+++ Load_fMRI_data.py
@@ -2,6 +2,8 @@
 
 from mvpa2.tutorial_suite import *
 
+tutorial_data_path = '/home/inb/santosg/TUTO/tutorial_data/data/'
+
 bold_fname = os.path.join(tutorial_data_path, 'haxby2001', 'sub001',
                               'BOLD', 'task001_run001', 'bold.nii.gz')
 ds = fmri_dataset(bold_fname)
